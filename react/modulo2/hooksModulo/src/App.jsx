84123f91e10c82e6489ae164f0e11a3cba6691f0
Lucas Gervasoni <jaxellucas@gmail.com>
2025-02-21 11:00:24 -0300

    adding more concepts about hooks

--- react/modulo2/hooksModulo/src/App.jsx
+++ react/modulo2/hooksModulo/src/App.jsx
@@ -1,28 +1,60 @@
-import { useState } from 'react'
-import Produtos from './Produto'
+import { useState, useEffect } from 'react'
+import Produto from './Produto'
 
+//PRATICANDO useState
 
-function App() {
-  const [dados, setDados] = useState(null);
-  const [loading, setLoading] = useState(null);
+// function App() {
+//   const [dados, setDados] = useState(null);
+//   const [loading, setLoading] = useState(null);
 
-  async function handleClick(event) {
-    setLoading(true);
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
-    const json = await response.json();
-    setDados(json);
-    setLoading(false);
+//   async function handleClick(event) {
+//     setLoading(true);
+//     const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+//     const json = await response.json();
+//     setDados(json);
+//     setLoading(false);
+//   }
+
+//   return (
+//       <div>
+//         <button style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
+//         <button style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
+//         <button style={{margin: '.5rem'}}onClick={handleClick}>smartphone</button>
+//         {loading && <p>Carregando...</p>}
+//         {!loading && dados && <Produtos dados={dados}/>}
+//       </div>
+//   )
+// }
+
+//PRATICANDO useEffect
+
+const App = () => {
+  const [produto, setProduto] = useState(null);
+
+  useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if (produtoLocal !== 'null') setProduto(produtoLocal);
+  }, []);
+
+  useEffect(() => {
+    if (produto !== null) window.localStorage.setItem('produto', produto);
+  }, [produto]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
   }
 
   return (
-      <div>
-        <button style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
-        <button style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
-        <button style={{margin: '.5rem'}}onClick={handleClick}>smartphone</button>
-        {loading && <p>Carregando...</p>}
-        {!loading && dados && <Produtos dados={dados}/>}
-      </div>
-  )
-}
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button style={{ marginRight: '1rem' }} onClick={handleClick}>
+        notebook
+      </button>
+      <button onClick={handleClick}>smartphone</button>
+      <Produto produto={produto} />
+    </div>
+  );
+};
+
 
 export default App
--- react/modulo2/hooksModulo/src/Produto.jsx
+++ react/modulo2/hooksModulo/src/Produto.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { useState, useEffect } from 'react'
 
-function Produtos({ dados }) {
+const Produto = ({ produto }) => {
+  const [dados, setDados] = useState(null);
+
+  useEffect(() => {
+    if (produto !== null) {
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+    }
+  }, [produto]);
+
+  if (dados === null) return null;
   return (
     <div>
       <h1>{dados.nome}</h1>
       <p>R$ {dados.preco}</p>
-      <img src={dados.fotos[0].src} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Produtos;
+export default Produto;
